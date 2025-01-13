a194b086472329b8ad4cd81e1a0013cb67194f36
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-13 10:05:39 -0800

    add Error page and default errorElement

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '../component/MainNavigation';
-import classes from './Root.module.css'
 
 function RootLayout() {
 	return <>
 		<MainNavigation />
 		{/* child routes are rendered via Outlet*/}
-		<main className={classes.content}>
+		<main>
 			<Outlet />
 		</main>
 	</>
